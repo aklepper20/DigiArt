6a48125838e8fbe256e190d4d86593738807962b
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-01-11 14:40:04 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Marketplace from "./pages/Marketplace";
+import Profile from "./pages/Profile";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Marketplace.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Marketplace() {
+  return (
+    <div>
+      <h1>MARKETPLACE</h1>
+    </div>
+  );
+}
+
+export default Marketplace;
