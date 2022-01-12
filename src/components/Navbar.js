96f2bb6c58e17d1fffa4a80d19c78980a70f09a5
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-01-11 16:53:51 -0800

    makes changes to profile page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,30 @@
 import React from "react";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import "../style/Navbar.css";
+
 function Navbar() {
-  return <div></div>;
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">
+        <img
+          src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+          alt="DigiArt Logo"
+        />
+        <h2>DigiArt</h2>
+      </div>
+
+      <div className="navbar__options">
+        <div className="navbar__list">
+          <p>Marketplace</p>
+          <p>Cart</p>
+          <p>My Profile</p>
+        </div>
+        <div className="navbar__wallet">
+          <AccountBalanceWalletIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,44 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import "../style/Profile.css";
+import Avatar from "@mui/material/Avatar";
 
 function Profile() {
   return (
-    <div>
-      <h1>PROFILE</h1>
+    <div className="profile">
+      <Navbar />
+
+      <div className="profile__top">
+        <div className="profile__user">
+          <Avatar />
+        </div>
+        <div className="profile__userDetails">
+          <p>Username</p>
+          <p># of items: 13</p>
+          <p>Amount of coin: 2.3</p>
+        </div>
+
+        <div className="profile__product">
+          <img
+            src="https://ca-times.brightspotcdn.com/dims4/default/6dee0c9/2147483647/strip/true/crop/3529x2604+0+0/resize/1486x1096!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fee%2F1f%2F66bec92d4cc6930855a95619237e%2Fla-jolla-cove.jpg"
+            alt="Product 1"
+          />
+          <div className="profile__">
+            <h4>Product Details</h4>
+            <button>edit</button>
+          </div>
+          <div className="profile__inputs">
+            <div className="profile__inputOne">
+              <input placeholder="Title" />
+              <input placeholder="Category" />
+            </div>
+            <div className="profile__inputTwo">
+              <input placeholder="Description" />
+              <input placeholder="Price" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
