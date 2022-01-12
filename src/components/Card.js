b493bda2bb5855107fe881a196e79ac9289889df
Nabzcreates <88214757+Nabzcreates@users.noreply.github.com>
2022-01-11 23:36:06 -0800

    fixed buttons hvers on marketplace

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,13 @@ function Card() {
   return (
     <div className='card'>
       <div className='topImg'>
-        xoxo
+        {/* xoxo image api should be pulled here */}
+        Image
       </div>
       <div className='description'>
-        price, name etc
+        <p>Price</p>
+        <p>Description</p> 
+        <p>Rating</p>
       </div>
     </div>
   )
