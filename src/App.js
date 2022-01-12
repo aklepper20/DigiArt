5fcfc07032de218028f314987a6f3bd835946457
Nabzcreates <88214757+Nabzcreates@users.noreply.github.com>
2022-01-11 19:21:55 -0800

    made landing page static

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {  BrowserRouter ,Routes, Route,} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Marketplace from "./pages/Marketplace";
 import Profile from "./pages/Profile";
@@ -7,16 +7,16 @@ import Cart from "./pages/Cart";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/marketplace" element={<Marketplace />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
-      </BrowserRouter>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,47 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import "../style/LandingPage.css";
+
 
 function LandingPage() {
   return (
-    <div>
-      <h2>LANDING PAGE </h2>
-
+  <div className='landingPage'>
+    <div className='landingNavbar'>
+      <div className="navbar__logo">
+        <img
+          src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+          alt="DigiArt Logo"
+        />
+        <h2>DigiArt</h2>
+      </div>
+      <div className='landingbtns'>
+        <button className= 'btns'id='signup'>Sign Up</button>
+        <button className= 'btns' id= 'login'>Login</button>
+      </div>
     </div>
+    <div className='container'>
+      <div className='blockRight'>
+        <p>Sell effortlessly using blockchain</p>
+        <p>Earn big and sell easy with any item</p>
+        <p id='smallerText'>Get ahead of the future...</p>
+        <button className='btns'>Sign Up</button>
+
+      </div>
+      <div className='blockLeft'>
+        <div className='leftImg'>NFT ART
+        {/* <img src="huh.jpeg" alt="nft"/> */}
+        </div>
+        <div className='leftImg'>Technology</div>
+        <div className='leftImg'>Everyday items</div>
+      </div>
+    </div>
+    <div className='footer'>
+      <div className='bannerImg'>ON1</div>
+      <div className='bannerImg'>Oxmons</div>
+      <div className='bannerImg'>33</div>
+      <div className='bannerImg'>3LAU</div>
+    </div>
+  </div>
   );
 }
 
