ac1f4a1a33d22c47ba94913f5327878182514718
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-01-11 22:47:41 -0800

    finishes styling for profile page and card page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,47 @@
 import React from "react";
+import CartCard from "../components/CartCard";
+import "../style/Cart.css";
+import Navbar from "../components/Navbar";
 
 function Cart() {
+  const objArray = [
+    {
+      img: "https://i.insider.com/61a7a6965d47cc0018e8ce17?width=600&format=jpeg&auto=webp",
+      price: 0.5,
+    },
+    {
+      img: "https://www.artnews.com/wp-content/uploads/2022/01/unnamed-2.png?w=631",
+      price: 0.2,
+    },
+    {
+      img: "https://1734811051.rsc.cdn77.org/data/images/full/392064/youtuber-shares-how-you-can-create-your-nft-on-iphone-heres-how-it-works.jpg",
+      price: 0.7,
+    },
+  ];
   return (
-    <div>
-      <h1>CART</h1>
-    </div>
+    <>
+      <Navbar />
+      <h1>Cart Items:</h1>
+      <div className="cart__container">
+        <div className="cart">
+          {objArray.map((obj) => (
+            <CartCard img={obj.img} price={obj.price} />
+          ))}
+        </div>
+
+        <div className="cart__checkout">
+          <div className="cart__total">
+            <p>
+              Wallet total: <span>.29eth</span>
+            </p>
+            <p>
+              Cart total: <span>2.4eth</span>
+            </p>
+          </div>
+          <button>Checkout</button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,44 +2,115 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import "../style/Profile.css";
 import Avatar from "@mui/material/Avatar";
+import NftCard from "../components/NftCard";
 
 function Profile() {
-  return (
-    <div className="profile">
-      <Navbar />
-      <h1>hi</h1>
-      <div className="profile__top">
-        <div className="profile__user">
-          <Avatar />
-        </div>
-        <div className="profile__userDetails">
-          <p>Username</p>
-          <p># of items: 13</p>
-          <p>Amount of coin: 2.3</p>
-        </div>
+  const objectArr = [
+    {
+      img: "https://www.artnews.com/wp-content/uploads/2022/01/unnamed-2.png?w=631",
+      id: 1934,
+    },
+    {
+      img: "https://1734811051.rsc.cdn77.org/data/images/full/392064/youtuber-shares-how-you-can-create-your-nft-on-iphone-heres-how-it-works.jpg",
+      id: 54359,
+    },
+    {
+      img: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F6170e01f8d7639b95a7f2eeb%2FSotheby-s-NFT-Natively-Digital-1-2-sale-Bored-Ape-Yacht-Club--8817-by-Yuga-Labs%2F0x0.png%3Ffit%3Dscale",
+      id: 2344,
+    },
+    {
+      img: "https://www.artnews.com/wp-content/uploads/2022/01/unnamed-2.png?w=631",
+      id: 1934,
+    },
+    {
+      img: "https://1734811051.rsc.cdn77.org/data/images/full/392064/youtuber-shares-how-you-can-create-your-nft-on-iphone-heres-how-it-works.jpg",
+      id: 54359,
+    },
+    {
+      img: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F6170e01f8d7639b95a7f2eeb%2FSotheby-s-NFT-Natively-Digital-1-2-sale-Bored-Ape-Yacht-Club--8817-by-Yuga-Labs%2F0x0.png%3Ffit%3Dscale",
+      id: 2344,
+    },
+    {
+      img: "https://www.artnews.com/wp-content/uploads/2022/01/unnamed-2.png?w=631",
+      id: 1934,
+    },
+    {
+      img: "https://1734811051.rsc.cdn77.org/data/images/full/392064/youtuber-shares-how-you-can-create-your-nft-on-iphone-heres-how-it-works.jpg",
+      id: 54359,
+    },
+    {
+      img: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F6170e01f8d7639b95a7f2eeb%2FSotheby-s-NFT-Natively-Digital-1-2-sale-Bored-Ape-Yacht-Club--8817-by-Yuga-Labs%2F0x0.png%3Ffit%3Dscale",
+      id: 2344,
+    },
+    {
+      img: "https://www.artnews.com/wp-content/uploads/2022/01/unnamed-2.png?w=631",
+      id: 1934,
+    },
+    {
+      img: "https://1734811051.rsc.cdn77.org/data/images/full/392064/youtuber-shares-how-you-can-create-your-nft-on-iphone-heres-how-it-works.jpg",
+      id: 54359,
+    },
+    {
+      img: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F6170e01f8d7639b95a7f2eeb%2FSotheby-s-NFT-Natively-Digital-1-2-sale-Bored-Ape-Yacht-Club--8817-by-Yuga-Labs%2F0x0.png%3Ffit%3Dscale",
+      id: 2344,
+    },
+    {
+      img: "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F6170e01f8d7639b95a7f2eeb%2FSotheby-s-NFT-Natively-Digital-1-2-sale-Bored-Ape-Yacht-Club--8817-by-Yuga-Labs%2F0x0.png%3Ffit%3Dscale",
+      id: 2344,
+    },
+  ];
 
-        <div className="profile__product">
-          <img
-            src="https://ca-times.brightspotcdn.com/dims4/default/6dee0c9/2147483647/strip/true/crop/3529x2604+0+0/resize/1486x1096!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2Fee%2F1f%2F66bec92d4cc6930855a95619237e%2Fla-jolla-cove.jpg"
-            alt="Product 1"
-          />
-          <div className="profile__title">
-            <h4>Product Details</h4>
-            <button>edit</button>
+  return (
+    <>
+      <Navbar />
+      <div className="profile">
+        <div className="profile__top">
+          <div className="profile__user">
+            <Avatar />
           </div>
-          <div className="profile__inputs">
-            <div className="profile__inputOne">
-              <input placeholder="Title" />
-              <input placeholder="Category" />
+
+          <div className="profile__userDetails">
+            <p>Username</p>
+            <p># of items: 13</p>
+            <p>Amount of coin: 2.3</p>
+          </div>
+
+          <div className="profile__product">
+            <img
+              src="https://i.insider.com/61a7a6965d47cc0018e8ce17?width=600&format=jpeg&auto=webp"
+              alt="Product 1"
+            />
+
+            <div className="profile__title">
+              <h4>Product Details:</h4>
+              <button>Edit</button>
             </div>
-            <div className="profile__inputTwo">
-              <input placeholder="Description" />
-              <input placeholder="Price" />
+
+            <div className="profile__inputs">
+              <div className="profile__inputOne">
+                <input placeholder="Title" className="profile__input" />
+                <input placeholder="Category" />
+              </div>
+              <div className="profile__inputTwo">
+                <input placeholder="Description" className="profile__input" />
+                <input placeholder="Price" />
+              </div>
             </div>
+            <div className="save-btn">
+              <button>Save</button>
+            </div>
+          </div>
+        </div>
+        <h1>Your Items:</h1>
+        <div className="profile__bottom">
+          <div className="profile__nftDisplay">
+            {objectArr.map((obj) => (
+              <NftCard img={obj.img} id={obj.id} />
+            ))}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
