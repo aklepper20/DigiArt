b8f0bb8e385afe63b7bfd288389ee5e6ab547407
Nabzcreates <88214757+Nabzcreates@users.noreply.github.com>
2022-01-12 19:30:57 -0800

    final touched for google auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../style/login.css'
+import GoogleIcon from '@mui/icons-material/Google';
 
 function Login({title, button, href, link, headerStatement, emailInput, passwordInput, btnFunction, googleFunction}) {
 
@@ -11,7 +12,7 @@ function Login({title, button, href, link, headerStatement, emailInput, password
         <input ref= {emailInput} className='login-email' type='email' placeholder='Email'/>
         <input ref= {passwordInput} className='login-password' type='password' placeholder='Password'/>
         <button onClick={btnFunction} className='login-button'>{button}</button>
-        <button onClick={googleFunction} className='google-button'>G</button>
+        <button id ='Gbtn'onClick={googleFunction} className='google-button'>{title} with  <GoogleIcon/></button>
 
         <div className='links'>
           <p>{headerStatement}</p>
--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,6 +1,6 @@
 import React, {useRef} from 'react'
 import Login from '../components/Login'
-import { auth, app } from '../utils/firebase'
+import { auth } from '../utils/firebase'
 import { signInWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 function Signin() {
 
@@ -9,25 +9,25 @@ function Signin() {
 
   const googleFunction = async()=>{
     const provider = new GoogleAuthProvider();
-   
-
     const auth = getAuth();
     signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        console.log(user)
-        if(user){
-          window.location = '/marketplace'
-        }
-      }).catch((error) => {
-        console.log(error);
-      });
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user)
+      const username = user.displayName;
 
-  }
+      if(user){
+        console.log(user)
+        window.location = '/marketplace'
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
+}
   const login = async()=>{
     console.log("hello")
     try{
