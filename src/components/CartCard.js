8493158498d790fb406828092f58b0e3c42d4dc6
jjesusteran-mori <iokoumorit@gmail.com>
2022-01-13 22:33:45 -0800

    minor changes to cart css

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -8,53 +8,52 @@ import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 
 function CartCard({ img, price }) {
-  console.log(img, price);
-  const [count, setCount] = useState(1);
+    console.log(img, price);
+    const [count, setCount] = useState(1);
 
-  return (
-    <div className="cart__product">
-      <img src={img} />
-      <div className="cart__coin">
-        <p>Coin: {price}</p>
-      </div>
-      <div className="cart__dropdown">
-        <Box
-          sx={{
-            minWidth: 120,
-            backgroundColor: "white",
-            borderRadius: "5px",
-            outline: "none",
-          }}
-        >
-          <FormControl fullWidth>
-            <InputLabel
-              id="demo-simple-select-label"
-              style={{ color: "black" }}
-            >
-              Quantity
-            </InputLabel>
-            <Select
-              style={{ color: "black" }}
-              className="cart__dropDown"
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={count}
-              label="Count"
-              onChange={(e) => setCount(e.target.value)}
-            >
-              <MenuItem value={1}>1</MenuItem>
-              <MenuItem value={2}>2</MenuItem>
-              <MenuItem value={3}>3</MenuItem>
-              <MenuItem value={4}>4</MenuItem>
-              <MenuItem value={5}>5</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      </div>
+    return (
+        <div className="cart__product">
+            <img src={img} />
+            <div className="cart__coin">
+                <p>Price: {price} ETH</p>
+            </div>
+            <div className="cart__dropdown">
+                QTY:
+                <Box
+                    sx={{
+                        minWidth: 50,
+                        borderRadius: "5px",
+                        outline: "none",
+                    }}
+                >
+                    <FormControl>
+                        {/* <InputLabel
+                            id="demo-simple-select-label"
+                            style={{ color: "black" }}
+                        >
+                            Quantity
+                        </InputLabel> */}
+                        <Select
+                            className="cart__dropDown"
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={count}
+                            label="Count"
+                            onChange={(e) => setCount(e.target.value)}
+                        >
+                            <MenuItem value={1}>1</MenuItem>
+                            <MenuItem value={2}>2</MenuItem>
+                            <MenuItem value={3}>3</MenuItem>
+                            <MenuItem value={4}>4</MenuItem>
+                            <MenuItem value={5}>5</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Box>
+            </div>
 
-      <DeleteIcon style={{ cursor: "pointer" }} />
-    </div>
-  );
+            <DeleteIcon style={{ cursor: "pointer" }} />
+        </div>
+    );
 }
 
 export default CartCard;
