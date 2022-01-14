c6434516101cf1740df5333317eeb76f1627db53
jjesusteran-mori <iokoumorit@gmail.com>
2022-01-14 11:43:20 -0800

    fixed

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,65 +1,62 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { Input } from '@mui/material';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Input } from "@mui/material";
 
 export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-  const handleSubmit = ()=>{
-    console.log("upload successful")
-  }
+    const handleSubmit = () => {
+        console.log("upload successful");
+    };
 
-  return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Upload
-      </Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Upload your Art!!!</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-           
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Name of the product"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Price"
-            type="number"
-            fullWidth
-            variant="standard"
-          />
-          <Input type="file" accept="image/*"  />
-
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSubmit}>Submit</Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+    return (
+        <div>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Upload
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Upload your Art!!!</DialogTitle>
+                <DialogContent>
+                    <DialogContentText></DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Name of the product"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Price"
+                        type="number"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <Input type="file" accept="image/*" />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleSubmit}>Submit</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -3,17 +3,16 @@ import "../style/LandingPage.css";
 import "../style/Card.css";
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
-import axios from 'axios';
+import axios from "axios";
 /////upload popup import below
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import { Input } from '@mui/material';
-
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Input } from "@mui/material";
 
 function Marketplace({ username }) {
     const [cryptoPunk, setCryptoPunk] = useState([]);
@@ -21,27 +20,25 @@ function Marketplace({ username }) {
     const [veeFriends, setVeeFriends] = useState([]);
     const [deadFellaz, setDeadFellaz] = useState([]);
 
+    //upload model below
+    const productNameRef = useRef();
+    const productPriceRef = useRef();
+    const productFileRef = useRef();
 
-//upload model below
-const productNameRef = useRef();
-const productPriceRef = useRef();
-const productFileRef = useRef();
+    const [open, setOpen] = React.useState(false);
 
-const [open, setOpen] = React.useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-const handleClickOpen = () => {
-  setOpen(true);
-};
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-const handleClose = () => {
-  setOpen(false);
-};
-
-const handleSubmit = ()=>{
-  console.log("upload successful")
-
-}
-///upload modal ends
+    const handleSubmit = () => {
+        console.log("upload successful");
+    };
+    ///upload modal ends
     const cryptopunk = {
         //*********crypotoPunk */
         method: "GET",
@@ -167,52 +164,6 @@ const handleSubmit = ()=>{
                         <div>Everyday Items</div>
                     </div>
 
-        {/* upload modal html begins here */}
-                    <div className="addItem">
-                                    
-                    <Button style={{width: '280px', height: '80px', ':hover': {
-                    bgcolor: 'pink',
-                    color: 'white',
-                    }}}  onClick={handleClickOpen}>
-                        Upload
-                    </Button>
-                    <Dialog open={open} onClose={handleClose}>
-                        <DialogTitle>Upload your Art!!!</DialogTitle>
-                        <DialogContent>
-                        <DialogContentText>
-                        
-                        </DialogContentText>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Name of the product"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            ref={productNameRef}
-                        />
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Price"
-                            type="number"
-                            fullWidth
-                            variant="standard"
-                            ref={productPriceRef}
-
-                        />
-                        <Input type="file" accept="image/*" ref={productFileRef} />
-
-                        </DialogContent>
-                        <DialogActions>
-                        <Button onClick={handleClose}>Cancel</Button>
-                        <Button onClick={handleSubmit}>Submit</Button>
-                        </DialogActions>
-                    </Dialog>
-                    </div>
-     {/* upload modal HTML ends here */}
                     {/* upload modal html begins here */}
                     <div className="addItem">
                         <Button
@@ -277,7 +228,6 @@ const handleSubmit = ()=>{
                                     nft.metadata.image ||
                                     nft.file_url
                                 }
-                                
                                 price={nft.price}
                                 cryptoPunk={cryptoPunk}
                             />
