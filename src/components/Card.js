90b4e6cd3c94fc53fa13f4e43d4ce4b070ec916c
jjesusteran-mori <iokoumorit@gmail.com>
2022-01-14 22:54:05 -0800

    using mui for popup box

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+// import PopUpCard from "./PopUpCard";
+import { styled, Box } from "@mui/system";
+import ModalUnstyled from "@mui/base/ModalUnstyled";
 import PopUpCard from "./PopUpCard";
 
-function Card({ name, img, description, price }) {
+const StyledModal = styled(ModalUnstyled)`
+    position: fixed;
+    z-index: 1300;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Backdrop = styled("div")`
+    z-index: -1;
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    -webkit-tap-highlight-color: transparent;
+`;
+
+const style = {
+    width: 480,
+    color: "white",
+    border: "2px solid #6bbaec",
+    p: 2,
+    px: 4,
+    pb: 3,
+};
+
+function Card({ name, img, description, price, key, id }) {
+    const [open, setOpen] = useState(false);
+    const handleClose = () => setOpen(false);
     return (
-        <div className="card">
+        <div className="card" key={key}>
             <div className="topImg">
-                {/* xoxo image api should be pulled here */}
                 <img src={img} alt="" />
             </div>
             <div className="description">
-                <p>
-                    {name} <span className="price">{price}</span>
-                </p>
+                <p>{name}</p>
+                <h6 className="price">{price}</h6>
                 <p>{description}</p>
-
-                <a className="button" href="#popup1">
-                    More Details
-                </a>
-                <PopUpCard name={name} img={img} description={description} />
             </div>
+            <button
+                type="button"
+                style={{
+                    backgroundColor: "lightskyblue",
+                    padding: "8px 12px",
+                    color: "white",
+                    width: "100px",
+                    display: "flex",
+                    margin: "10px auto 20px",
+                    textAlign: "center",
+                }}
+                onClick={() => setOpen(true)}
+            >
+                Checkout
+            </button>
+            <StyledModal
+                aria-labelledby="unstyled-modal-title"
+                aria-describedby="unstyled-modal-description"
+                open={open}
+                onClose={handleClose}
+                BackdropComponent={Backdrop}
+            >
+                <Box sx={style}>
+                    <div
+                        style={{
+                            width: "480px",
+                            display: "grid",
+                            placeItems: "center",
+                        }}
+                    >
+                        <PopUpCard
+                            name={name}
+                            img={img}
+                            description={description}
+                        />
+                    </div>
+                </Box>
+            </StyledModal>
         </div>
     );
 }
