72adbbe7fa1f8d31f12273e0687e3de1af612a8d
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-01-16 16:34:00 -0800

    adds reducer functionality

---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+import { ContactSupportOutlined } from "@mui/icons-material";
+
+export const initialState = {
+  basket: [],
+  // user: null,
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+
+    case "REMOVE_FROM_BASKET":
+      let newBasket = [...state.basket];
+      const index = state.basket.findIndex((item) => item.id === action.id);
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Cannot remove digital asset, id: ${action.id}`);
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
