7207d820ab243c986d69390da6b649d9ea120602
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-01-16 16:36:28 -0800

    adds links to pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,126 +7,124 @@ import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import CloseIcon from "@mui/icons-material/Close";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import { useStateValue } from "../StateProvider";
 
 function Navbar() {
-    const [state, setState] = useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
+  const [{ basket }] = useStateValue();
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
-        setState({ ...state, [anchor]: open });
-    };
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
 
-    const list = (anchor) => (
-        <Box
-            sx={{
-                width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
-                height: "100%",
-            }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <div className="navbar__drawer">
-                <div className="navbar__drawerTop">
-                    <div>Account</div>
-                    <p>ID: 45235</p>
-                </div>
-                <div className="navbar__drawerBottom">
-                    <div className="navbar__eth">
-                        <img
-                            src="https://cryptologos.cc/logos/ethereum-eth-logo.png"
-                            alt="ethereum logo"
-                        />
-                    </div>
-                    <p className="eth__number">Eth: {0.3}</p>
-                    <p className="eth__username">@user</p>
-                </div>
-            </div>
-        </Box>
-    );
-
-    return (
-        <div className="navbar">
-            <div className="navbar__logo">
-                <img
-                    src="https://cdn.logo.com/hotlink-ok/logo-social.png"
-                    alt="DigiArt Logo"
-                />
-                <h2>
-                    <a href="/">DigiArt</a>
-                </h2>
-            </div>
-
-            <div className="navbar__options">
-                <div className="navbar__list">
-                    <ul>
-                        <li>
-                            <a href="/marketplace">Marketplace</a>
-                        </li>
-                        <li>
-                            <Link to="/cart">
-                                <div style={{ display: "flex" }}>
-                                    <a href="/cart">Cart</a>
-                                    <ShoppingBagIcon
-                                        style={{ color: "yellow" }}
-                                    />
-                                </div>
-                            </Link>
-                        </li>
-                        <li>
-                            <a href="/profile">My Profile</a>
-                        </li>
-                        <li>
-                            <div className="navbar__wallet">
-                                <div>
-                                    {["right"].map((anchor) => (
-                                        <React.Fragment key={anchor}>
-                                            <Button
-                                                onClick={toggleDrawer(
-                                                    anchor,
-                                                    true
-                                                )}
-                                            >
-                                                <AccountBalanceWalletIcon />
-                                            </Button>
-                                            <Drawer
-                                                PaperProps={{
-                                                    style: {
-                                                        height: "30vh",
-                                                        borderRadius:
-                                                            "10px 0 0 10px",
-                                                        marginTop: "60px",
-                                                    },
-                                                }}
-                                                anchor={anchor}
-                                                open={state[anchor]}
-                                                onClose={toggleDrawer(
-                                                    anchor,
-                                                    false
-                                                )}
-                                            >
-                                                {list(anchor)}
-                                            </Drawer>
-                                        </React.Fragment>
-                                    ))}
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+  const list = (anchor) => (
+    <Box
+      sx={{
+        width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+        height: "100%",
+      }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <div className="navbar__drawer">
+        <div className="navbar__drawerTop">
+          <div>Account</div>
+          <p>ID: 45235</p>
         </div>
-    );
+        <div className="navbar__drawerBottom">
+          <div className="navbar__eth">
+            <img
+              src="https://cryptologos.cc/logos/ethereum-eth-logo.png"
+              alt="ethereum logo"
+            />
+          </div>
+          <p className="eth__number">Eth: {0.3}</p>
+          <p className="eth__username">@user</p>
+        </div>
+      </div>
+    </Box>
+  );
+
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">
+        <img
+          src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+          alt="DigiArt Logo"
+        />
+        <h2>
+          <Link to="/">
+            <a href="/">DigiArt</a>
+          </Link>
+        </h2>
+      </div>
+
+      <div className="navbar__options">
+        <div className="navbar__list">
+          <ul>
+            <li>
+              <Link to="/marketplace">
+                <a href="/marketplace">Marketplace</a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart">
+                <div style={{ display: "flex" }}>
+                  <a href="/cart">Cart</a>
+                  <ShoppingBagIcon style={{ color: "yellow" }} />
+                  <div style={{ margin: "0px 5px" }}>{basket?.length}</div>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile">
+                <a href="/profile">My Profile</a>
+              </Link>
+            </li>
+            <li>
+              <div className="navbar__wallet">
+                <div>
+                  {["right"].map((anchor) => (
+                    <React.Fragment key={anchor}>
+                      <Button onClick={toggleDrawer(anchor, true)}>
+                        <AccountBalanceWalletIcon />
+                      </Button>
+                      <Drawer
+                        PaperProps={{
+                          style: {
+                            height: "30vh",
+                            borderRadius: "10px 0 0 10px",
+                            marginTop: "60px",
+                          },
+                        }}
+                        anchor={anchor}
+                        open={state[anchor]}
+                        onClose={toggleDrawer(anchor, false)}
+                      >
+                        {list(anchor)}
+                      </Drawer>
+                    </React.Fragment>
+                  ))}
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
