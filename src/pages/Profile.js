866a1cbfd531e124fa7d48906797a1cae2375060
jjesusteran-mori <iokoumorit@gmail.com>
2022-01-17 21:21:24 -0800

    styled navbar

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,39 +6,42 @@ import NftCard from "../components/NftCard";
 import ProfileProductDetails from "../components/ProfileProductDetails";
 
 function Profile({ randomUserCoin, mrkt }) {
-  const [selectedNft, setSelectedNft] = useState(0);
-  // const [editInputs, setEditInputs] = useState(false);
-  // const [activeNft, setActiveNft] = useState(mrkt[0]);
-  // useEffect(() => {
-  //   setActiveNft(mrkt[selectedNft]);
-  // }, [mrkt, selectedNft]);
+    const [selectedNft, setSelectedNft] = useState(0);
+    // const [editInputs, setEditInputs] = useState(false);
+    // const [activeNft, setActiveNft] = useState(mrkt[0]);
+    // useEffect(() => {
+    //   setActiveNft(mrkt[selectedNft]);
+    // }, [mrkt, selectedNft]);
 
-  return (
-    <>
-      <Navbar randomUserCoin={randomUserCoin} />
+    return (
+        <>
+            <Navbar randomUserCoin={randomUserCoin} />
 
-      {mrkt.length > 0 && (
-        <div className="profile">
-          <div className="profile__top">
-            <div className="profile__user">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz7xkKmOxNZeatDKzjesmJG-s2clYi3gsncQ&usqp=CAU"
-                alt=""
-              />
-              <h2>Kwesi</h2>
-              <p>@KwesiB</p>
-              <p>eth: {randomUserCoin}</p>
-            </div>
+            {mrkt.length > 0 && (
+                <div className="profile">
+                    <div className="profile__top">
+                        <div className="profile__user">
+                            <img
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz7xkKmOxNZeatDKzjesmJG-s2clYi3gsncQ&usqp=CAU"
+                                alt=""
+                            />
+                            <h2>Kwesi</h2>
+                            <p>@KwesiB</p>
+                            <p>eth: {randomUserCoin}</p>
+                        </div>
 
-            <div className="profile__item">
-              <div className="profile__name">
-                <h2>PRODUCT DETAILS</h2>
-              </div>
+                        <div className="profile__item">
+                            <div className="profile__name">
+                                <h2>PRODUCT DETAILS</h2>
+                            </div>
 
-              {mrkt && (
-                <ProfileProductDetails mrkt={mrkt} selectedNft={selectedNft} />
-              )}
-              {/* <div className="profile__itemDesc">
+                            {mrkt && (
+                                <ProfileProductDetails
+                                    mrkt={mrkt}
+                                    selectedNft={selectedNft}
+                                />
+                            )}
+                            {/* <div className="profile__itemDesc">
               <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8_Sw6z7D4DQY765mD8KyTzXPnvKyNRSrfSQ&usqp=CAU"
                 alt="NFT"
@@ -54,23 +57,28 @@ function Profile({ randomUserCoin, mrkt }) {
                 </div>
               </div>
             </div> */}
-            </div>
-          </div>
+                        </div>
+                    </div>
 
-          <h2 style={{ margin: "35px 15px 15px 45px" }}>Your Items</h2>
-          <div className="profile__bottom">
-            <div className="profile__nftDisplay">
-              {mrkt.map((obj) => (
-                <div onClick={() => setSelectedNft(obj.id)} className="nftCard">
-                  <NftCard img={obj.image_url} ID={obj.id} />
+                    <h2 style={{ margin: "35px 15px 15px 45px" }}>
+                        Your Items
+                    </h2>
+                    <div className="profile__bottom">
+                        <div className="profile__nftDisplay">
+                            {mrkt.map((obj) => (
+                                <div
+                                    onClick={() => setSelectedNft(obj.id)}
+                                    className="nftCard"
+                                >
+                                    <NftCard img={obj.image_url} ID={obj.id} />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+            )}
+        </>
+    );
 }
 
 export default Profile;
