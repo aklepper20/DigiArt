741595241a6bfa1d936c59443ded17282b313c6e
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-01-19 13:50:53 -0800

    updates

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,10 @@
 import React, { useState, useRef } from "react";
 import db, { auth } from "../utils/firebase";
 import {
-    signInWithEmailAndPassword,
-    getAuth,
-    signInWithPopup,
-    GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
 } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -12,191 +12,179 @@ import GoogleIcon from "@mui/icons-material/Google";
 import "../style/login.css";
 
 function Auth({ loginRedboxClass }) {
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    const NameInput = useRef();
-    const loginEmailRef = useRef();
-    const loginPasswordRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const NameInput = useRef();
+  const loginEmailRef = useRef();
+  const loginPasswordRef = useRef();
 
-    const googleFunction = async (e) => {
-        e.preventDefault();
-        const provider = new GoogleAuthProvider();
-        const auth = getAuth();
-        try {
-            signInWithPopup(auth, provider).then(async (result) => {
-                const user = result.user;
-                const username = user.displayName;
-                // console.log(username)
-                // console.log(user.email
-                await setDoc(doc(db, "users", `${user.uid}`), {
-                    userData: {
-                        emailID: user.email,
-                        name: username,
-                    },
-                });
-                if (user) {
-                    window.location = "/marketplace";
-                }
-            });
-        } catch (error) {
-            alert(error.message);
+  const googleFunction = async (e) => {
+    e.preventDefault();
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    try {
+      signInWithPopup(auth, provider).then(async (result) => {
+        const user = result.user;
+        const username = user.displayName;
+        // console.log(username)
+        // console.log(user.email
+        await setDoc(doc(db, "users", `${user.uid}`), {
+          userData: {
+            emailID: user.email,
+            name: username,
+          },
+        });
+        if (user) {
+          window.location = "/marketplace";
         }
-    };
-    const login = async () => {
-        try {
-            await signInWithEmailAndPassword(
-                auth,
-                loginEmailRef.current.value,
-                loginPasswordRef.current.value
-            ).then((user) => {
-                if (user) {
-                    window.location = "/marketplace";
-                }
-            });
-        } catch (error) {
-            alert(error.message, "error");
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        loginEmailRef.current.value,
+        loginPasswordRef.current.value
+      ).then((user) => {
+        if (user) {
+          window.location = "/marketplace";
         }
-    };
-    const register = async (e) => {
-        e.preventDefault();
-        try {
-            await createUserWithEmailAndPassword(
-                auth,
-                emailRef.current.value,
-                passwordRef.current.value
-            ).then(async (cred) => {
-                // console.log(cred)
-                await setDoc(doc(db, "users", `${cred.user.uid}`), {
-                    userData: {
-                        emailID: emailRef.current.value,
-                        name: NameInput.current.value,
-                    },
-                });
-                if (cred) {
-                    window.location = "/marketplace";
-                }
-            });
-        } catch (error) {
-            alert(error.message);
+      });
+    } catch (error) {
+      alert(error.message, "error");
+    }
+  };
+  const register = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        emailRef.current.value,
+        passwordRef.current.value
+      ).then(async (cred) => {
+        // console.log(cred)
+        await setDoc(doc(db, "users", `${cred.user.uid}`), {
+          userData: [
+            {
+              emailID: emailRef.current.value,
+              name: NameInput.current.value,
+            },
+          ],
+        });
+        if (cred) {
+          window.location = "/marketplace";
         }
-    };
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
-    const [redboxClass, setRedboxClass] = useState("");
-    const toggleRedbox = (e) => {
-        e.preventDefault();
-        if (redboxClass === "") {
-            setRedboxClass("right-panel-active");
-        } else if (redboxClass === "right-panel-active") {
-            setRedboxClass("");
-        }
-    };
-    return (
-        <div className="outer-form__container">
-            <div className={`form-container ${redboxClass}`}>
-                <form action="" className="loginContainer">
-                    <div className="title-container">
-                        <h1>Login</h1>
-                    </div>
-                    <input
-                        ref={loginEmailRef}
-                        className="login-email"
-                        type="email"
-                        placeholder="Email"
-                    />
-                    <input
-                        ref={loginPasswordRef}
-                        className="login-password"
-                        type="password"
-                        placeholder="Password"
-                    />
-                    <button onClick={login} className="loginBtn">
-                        Login
-                    </button>
-                    <button
-                        id="Gbtn"
-                        onClick={googleFunction}
-                        className="loginBtn"
-                    >
-                        <GoogleIcon />
-                    </button>
-                    <div className="links">
-                        <p>Need an account?</p>
-                        <a href="/signup">Sign up</a>
-                    </div>
-                </form>
-                <form action="" className="register-form-container">
-                    <div className="title-container">
-                        <h1>Sign UP</h1>
-                    </div>
-                    <input
-                        ref={NameInput}
-                        className="login-name"
-                        type="text"
-                        placeholder="Enter your name"
-                    />
-                    <input
-                        ref={emailRef}
-                        className="login-email"
-                        type="email"
-                        placeholder="Email"
-                    />
-                    <input
-                        ref={passwordRef}
-                        className="login-password"
-                        type="password"
-                        placeholder="Password"
-                    />
-                    <button onClick={register} className="loginBtn">
-                        Sign Up
-                    </button>
-                    <button
-                        id="Gbtn"
-                        onClick={googleFunction}
-                        className="loginBtn"
-                    >
-                        <GoogleIcon />
-                    </button>
-                    <div className="links">
-                        <p>Already have an account?</p>
-                        <a href="/login">Login</a>
-                    </div>
-                </form>
-                <div className="overlay-container">
-                    <div className="auth-overlay">
-                        <div className="overlay-panel overlay-left">
-                            <h1>Welcome Back!</h1>
-                            <p>
-                                Stay connected with us please login with your
-                                personal info
-                            </p>
-                            <button
-                                className="form-toggle-btn"
-                                id="signIn"
-                                onClick={(e) => toggleRedbox(e)}
-                            >
-                                Sign In
-                            </button>
-                        </div>
-                        <div className="overlay-panel overlay-right">
-                            <h1>Hello, Friend!</h1>
-                            <p>
-                                Enter your personal details and start journey
-                                with us
-                            </p>
-                            <button
-                                className="form-toggle-btn"
-                                id="signUp"
-                                href="/signup"
-                                onClick={(e) => toggleRedbox(e)}
-                            >
-                                Sign Up
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  const [redboxClass, setRedboxClass] = useState("");
+  const toggleRedbox = (e) => {
+    e.preventDefault();
+    if (redboxClass === "") {
+      setRedboxClass("right-panel-active");
+    } else if (redboxClass === "right-panel-active") {
+      setRedboxClass("");
+    }
+  };
+  return (
+    <div className="outer-form__container">
+      <div className={`form-container ${redboxClass}`}>
+        <form action="" className="loginContainer">
+          <div className="title-container">
+            <h1>Login</h1>
+          </div>
+          <input
+            ref={loginEmailRef}
+            className="login-email"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            ref={loginPasswordRef}
+            className="login-password"
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={login} className="loginBtn">
+            Login
+          </button>
+          <button id="Gbtn" onClick={googleFunction} className="loginBtn">
+            <GoogleIcon />
+          </button>
+          <div className="links">
+            <p>Need an account?</p>
+            <a href="/signup">Sign up</a>
+          </div>
+        </form>
+        <form action="" className="register-form-container">
+          <div className="title-container">
+            <h1>Sign UP</h1>
+          </div>
+          <input
+            ref={NameInput}
+            className="login-name"
+            type="text"
+            placeholder="Enter your name"
+          />
+          <input
+            ref={emailRef}
+            className="login-email"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            ref={passwordRef}
+            className="login-password"
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={register} className="loginBtn">
+            Sign Up
+          </button>
+          <button id="Gbtn" onClick={googleFunction} className="loginBtn">
+            <GoogleIcon />
+          </button>
+          <div className="links">
+            <p>Already have an account?</p>
+            <a href="/login">Login</a>
+          </div>
+        </form>
+        <div className="overlay-container">
+          <div className="auth-overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Welcome Back!</h1>
+              <p>Stay connected with us please login with your personal info</p>
+              <button
+                className="form-toggle-btn"
+                id="signIn"
+                onClick={(e) => toggleRedbox(e)}
+              >
+                Sign In
+              </button>
             </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Enter your personal details and start journey with us</p>
+              <button
+                className="form-toggle-btn"
+                id="signUp"
+                href="/signup"
+                onClick={(e) => toggleRedbox(e)}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
