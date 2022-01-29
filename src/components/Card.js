b521cba0264124c3403f21a33316742e7869b6a0
Aly Baez <alybaez@Alys-MBP.home>
2022-01-29 10:45:52 -0800

    checks bid bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,128 +9,127 @@ import Bid from "./Bid";
 import { useStateValue } from "../StateProvider";
 
 const StyledModal = styled(ModalUnstyled)`
-    position: fixed;
-    z-index: 1300;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    left: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  position: fixed;
+  z-index: 1300;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Backdrop = styled("div")`
-    z-index: -1;
-    position: fixed;
-    right: 0;
-    bottom: 0;
-    top: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.6);
-    -webkit-tap-highlight-color: transparent;
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  -webkit-tap-highlight-color: transparent;
 `;
 
 const style = {
-    width: 480,
-    color: "white",
-    border: "2px solid #6bbaec",
-    p: 2,
-    px: 4,
-    pb: 3,
+  width: 480,
+  color: "white",
+  border: "2px solid #6bbaec",
+  p: 2,
+  px: 4,
+  pb: 3,
 };
 
 function Card({
-    name,
-    img,
-    description,
-    price,
-    key,
-    id,
-    nftSymbol,
-    openSeaLink,
+  name,
+  img,
+  description,
+  price,
+  key,
+  id,
+  nftSymbol,
+  openSeaLink,
 }) {
-    const [{ basket }, dispatch] = useStateValue();
-    const [open, setOpen] = useState(false);
-    const handleClose = () => setOpen(false);
-    const [bidOpen, setBidOpen] = useState(false);
+  const [{ basket }, dispatch] = useStateValue();
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+  const [bidOpen, setBidOpen] = useState(false);
 
-    const addToBasket = () => {
-        dispatch({
-            type: "ADD_TO_BASKET",
-            item: {
-                id: id,
-                name: name,
-                img: img,
-                price: price,
-                qty: 1,
-            },
-        });
-    };
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        name: name,
+        img: img,
+        price: price,
+        qty: 1,
+      },
+    });
+  };
 
-    // const bidInfo = () => {
-    //   setBidOpen(true);
-    //   dispatch({
-    //     type: "BID",
-    //     item: {
-    //       price: price,
-    //     },
-    //   });
-    // };
+  const bidInfo = () => {
+    setBidOpen(true);
+    dispatch({
+      type: "BID",
+      item: {
+        price: price,
+      },
+    });
+  };
 
-    return (
-        <div className="card" key={key}>
-            <div className="topImg">
-                <img src={img} alt="" />
-            </div>
-            <div className="description">
-                <p className="card-name">{name}</p>
-                <h6 className="price">{price}</h6>
-                <p>{description}</p>
-            </div>
-            <div className="card-btns">
-                {/* ********** bid feature ********** */}
-                {/* <RequestQuoteIcon onClick={bidInfo} /> */}
-                {/* {bidOpen === true && <Bid setBidOpen={setBidOpen} />} */}
-                <button
-                    className="moredts"
-                    type="button"
-                    style={{
-                        color: "white",
-                    }}
-                    onClick={() => setOpen(true)}
-                >
-                    More Details...
-                </button>
-                <ShoppingCartIcon onClick={addToBasket} />
-            </div>
-            <StyledModal
-                aria-labelledby="unstyled-modal-title"
-                aria-describedby="unstyled-modal-description"
-                open={open}
-                onClose={handleClose}
-                BackdropComponent={Backdrop}
-            >
-                <Box sx={style}>
-                    <div
-                        style={{
-                            display: "grid",
-                            placeItems: "center",
-                        }}
-                    >
-                        <PopUpCard
-                            name={name}
-                            img={img}
-                            description={description}
-                            price={price}
-                            nftSymbol={nftSymbol}
-                            openSeaLink={openSeaLink}
-                        />
-                    </div>
-                </Box>
-            </StyledModal>
-        </div>
-    );
+  return (
+    <div className="card" key={key}>
+      <div className="topImg">
+        <img src={img} alt="" />
+      </div>
+      <div className="description">
+        <p className="card-name">{name}</p>
+        <h6 className="price">{price}</h6>
+        <p>{description}</p>
+      </div>
+      <div className="card-btns">
+        <RequestQuoteIcon onClick={bidInfo} />
+        {bidOpen && <Bid setBidOpen={setBidOpen} />}
+        <button
+          className="moredts"
+          type="button"
+          style={{
+            color: "white",
+          }}
+          onClick={() => setOpen(true)}
+        >
+          More Details...
+        </button>
+        <ShoppingCartIcon onClick={addToBasket} />
+      </div>
+      <StyledModal
+        aria-labelledby="unstyled-modal-title"
+        aria-describedby="unstyled-modal-description"
+        open={open}
+        onClose={handleClose}
+        BackdropComponent={Backdrop}
+      >
+        <Box sx={style}>
+          <div
+            style={{
+              display: "grid",
+              placeItems: "center",
+            }}
+          >
+            <PopUpCard
+              name={name}
+              img={img}
+              description={description}
+              price={price}
+              nftSymbol={nftSymbol}
+              openSeaLink={openSeaLink}
+            />
+          </div>
+        </Box>
+      </StyledModal>
+    </div>
+  );
 }
 
 export default Card;
