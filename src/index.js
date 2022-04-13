e286b6f32805def84b5a048979dc68b33784e439
Aly Baez <alybaez@Alys-MacBook-Pro.local>
2022-04-13 12:02:34 -0700

    fixes api issue for generating user card

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
 import { StateProvider } from "./StateProvider";
 import reducer, { initialState } from "./reducer";
 
